f388d520c4973d27c71ac59475834bcf8157478f
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-25 12:02:16 +0530

    Create BruteForce_GCD_LCM.cpp

---
+++ CPP/Maths DSA/BruteForce_GCD_LCM.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute force approach
+int gcd(int a,int b){
+	if(a==0){
+		return b;
+	}
+	if(b==0){
+		return a;
+	}
+	while(a!=b){
+		if(a>b){
+			a=a-b;
+		}
+		else b=b-a;
+	}
+	return a;
+}
+
+int main(){
+
+	int a,b;
+	cin>>a>>b;
+	cout<<"GCD "<<gcd(a,b)<<endl;
+	cout<<"LCM "<<a*b/gcd(a,b)<<endl;
+}
