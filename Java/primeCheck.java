a34762962c902841b26ef4e2ce08f28b83d1dc8e
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-26 02:25:34 +0530

    Create primeCheck.java

---
+++ Java/primeCheck.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MyClass {
+   static boolean primeCheck(int n){
+       if(n<=1){
+           return false;
+       }
+       for(int i=2;i<n;i++){
+           if(n%i==0){
+               return false;
+           }
+       }return true;
+   }
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      if(primeCheck(a)){
+          System.out.println("True");
+      }
+      else System.out.println("False");
+    }
+}
