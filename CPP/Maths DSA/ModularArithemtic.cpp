4d75abf3b89f2f2bd0a7f6c3e0f6d2adc8d17ebd
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-25 12:25:44 +0530

    Update ModularArithemtic.cpp

--- CPP/Maths DSA/ModularArithemtic.cpp
+++ CPP/Maths DSA/ModularArithemtic.cpp
@@ -10,8 +10,9 @@ First off, some important identities about the modulo operator:
 int modularExponentiation(int x, int n, int m) {
 	int res=1;
     while(n>0){
-        if(n%2){
-            res=(1LL*((res%m)*x)%m)%m;
+	      
+        if(n%2){  //oddPower
+            res=(1LL*res*(x%m)%m);
         }
         n=n>>1;
         x=(1LL*(x%m)*(x%m))%m;
