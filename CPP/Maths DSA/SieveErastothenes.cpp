c08f8e96b44a0e2755cc66c3e919a68de92ab492
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-25 11:55:55 +0530

    Create SieveErastothenes.cpp

---
+++ CPP/Maths DSA/SieveErastothenes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int ct=0;
+        vector<bool>prime(n+1,true);
+        prime[0]=false;
+        prime[1]=true;
+        
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                ct++;
+                for(int j=2*i;j<=n;j=j+i){
+                    prime[j]=false;
+                }
+            }
+        }
+        return ct;
+    }
+};
