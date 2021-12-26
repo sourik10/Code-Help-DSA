da6407e9632c8aa048af1f89430fd2d7e156ced9
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-26 21:09:56 +0530

    Update StringPalindromeCheck.cpp

--- CPP/Strings/StringPalindromeCheck.cpp
+++ CPP/Strings/StringPalindromeCheck.cpp
@@ -6,10 +6,10 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-    for (int i = 0, j = s.size() - 1; i < j;i++,j--) { // Move 2                                     pointers from each end until they collide
-        while (isalnum(s[i]) == false && i < j) i++; // Increment left                                          pointer if not alphanumeric
-        while (isalnum(s[j]) == false && i < j) j--; // Decrement right                                         pointer if no alphanumeric
-        if (toupper(s[i]) != toupper(s[j])) return false; // Exit and                                       return error if not match
+    for (int i = 0, j = s.size() - 1; i < j;i++,j--) {     	// Move 2 pointers from each end until they collide
+        while (isalnum(s[i]) == false && i < j) i++; 		// Increment left pointer if not alphanumeric
+        while (isalnum(s[j]) == false && i < j) j--; 		// Decrement right pointer if no alphanumeric
+        if (toupper(s[i]) != toupper(s[j])) return false; 	// Exit and return error if not match
     }
     
     return true;
