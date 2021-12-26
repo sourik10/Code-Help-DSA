88919a43fedd0b03cccc7c8efa29cb009d63ad6e
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-26 20:36:26 +0530

    Update StringPalindromeCheck.cpp

--- CPP/Strings/StringPalindromeCheck.cpp
+++ CPP/Strings/StringPalindromeCheck.cpp
@@ -1,12 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+char toLowercase(char ch){
+	if(ch>='a' and ch<='z'){
+		return ch;
+	}
+	else {  //Uppercase to Lowercase conversion
+		ch=ch-'A'+'a';
+		return ch;
+	}
+}
+
 bool checkPalindrome(string s)
 {
     int l=0;
     int h=s.length()-1;
     while(l<=h){
-        if(s[l]!=s[h]){
+        if(toLowercase(s[l])!=toLowercase(s[h])){
             return false;
         }
         else{
@@ -15,8 +25,9 @@ bool checkPalindrome(string s)
         }
     }return true;
 }
+
 int main(){
 
-	string s="12221";
+	string s="SouuuuuoS";
 	cout<<checkPalindrome(s)<<endl;
 }
