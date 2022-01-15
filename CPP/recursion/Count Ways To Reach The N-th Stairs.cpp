b0838086cd0d6275e07bd53ddadef77792f7a440
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-15 21:48:26 +0530

    Update Count Ways To Reach The N-th Stairs.cpp

--- CPP/recursion/Count Ways To Reach The N-th Stairs.cpp
+++ CPP/recursion/Count Ways To Reach The N-th Stairs.cpp
@@ -1,9 +1,14 @@
+//Count Ways To Reach The N-th Stairs
+//https://www.codingninjas.com/codestudio/problems/count-ways-to-reach-nth-stairs_798650?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_10&leftPanelTab=0
+
+#include<bits/stdc++.h>
+using namespace std;
+
 int fib(int n){
     //base case
     if(n==0 || n==1){
         return n;
     }
-    
     //recursive case
     return fib(n-1)+fib(n-2);
 }
@@ -11,4 +16,18 @@ int fib(int n){
 int countDistinctWayToClimbStair(long long nStairs)
 {
    return fib(nStairs+1);
+}
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n;cin>>n;
+		cout<<countDistinctWayToClimbStair(n)<<endl;
+
+	}
+
+	return 0;
+
 }
