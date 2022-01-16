a982f83d2b263c1a05145fe81fafd09c619b6b91
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-16 13:21:48 +0530

    Update quickSort Algortihm .cpp

--- CPP/recursion/quickSort Algortihm .cpp
+++ CPP/recursion/quickSort Algortihm .cpp
@@ -6,63 +6,52 @@
 using namespace std;
 
 
-int partition( int arr[], int s, int e) {
-
-    int pivot = arr[s]; //taking first element as pivot 
-
-    int cnt = 0;
-    for(int i = s+1; i<=e; i++) {
-        if(arr[i] <=pivot) {
-            cnt++;
+int partition(vector<int>&ar,int s,int e){
+    int pivot=ar[s]; //taking first element as pivot 
+    int ct=0;
+    for(int i=s+1;i<=e;i++){
+        if(ar[i]<=pivot){
+            ct++;
         }
     }
-    int pivotIndex = s + cnt;
-    swap(arr[pivotIndex], arr[s]);
-
-   
-
-    int i = s, j = e;
-
-    while(i < pivotIndex && j > pivotIndex) {
-
-        while(arr[i] <= pivot) 
-        {
+    //pivot index at correct position
+    int pivotIndex=s+ct;
+    swap(ar[pivotIndex],ar[s]);
+    
+    //upto this 
+    //element present on left side are smaller than pivot
+    //element present on right side are greater than pivot
+    
+    int i=s,j=e;
+    while(i<pivotIndex and j>pivotIndex){
+        while(ar[i]<=pivot){
             i++;
         }
-
-        while(arr[j] > pivot) {
+        while(ar[j]>pivot){
             j--;
         }
-
-        if(i < pivotIndex && j > pivotIndex) {
-            swap(arr[i++], arr[j--]);
+        //swap needed 
+        if(i<pivotIndex and j>pivotIndex){
+            swap(ar[i++],ar[j--]);
         }
-
-    }
-
-    return pivotIndex;
-
+    }return pivotIndex;
+}
+void quickSort(vector<int>&ar,int s,int e){
+    if(s>=e) return;
+    
+    int p=partition(ar,s,e);
+    quickSort(ar,s,p-1);
+    quickSort(ar,p+1,e);
 }
 
-void quickSort(int arr[], int s, int e) {
-
-    //base case
-    if(s >= e) 
-        return ;
-
-    int p = partition(arr, s, e);
-
-    quickSort(arr, s, p-1); //left subarray
-    quickSort(arr, p+1, e); //right subarray
-
-}
 
 int main() {
 
-    int arr[10] = {2,4,1,6,9 ,9,9,9,9,9};
+   /* int arr[10] = {2,4,1,6,9 ,9,9,9,9,9};
     int n = 10;
-
-    quickSort(arr, 0, n-1);
+    */
+    vector<int>arr{2,4,1,6,9 ,9,9,9,9,9};
+    quickSort(arr, 0, arr.size()-1);
 
     for(int i=0; i<n; i++) 
     {
