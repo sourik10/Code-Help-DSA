d4e733417df5c61d4f7b88a1862be76301fcd7c7
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-02-11 22:38:47 +0530

    Update two stack in a array.cpp

--- CPP/stack/two stack in a array.cpp
+++ CPP/stack/two stack in a array.cpp
@@ -1,28 +1,29 @@
 class TwoStack {
 
 public:
-
    int top1,top2;
     int *arr;
     int size;
+    
     TwoStack(int size) {
         this->size=size;
-        this->top1=-1;
+        this->top1=-1; 
         this->top2=size;
         arr=new int[size];
     }
     
     // Push in stack 1.
     void push1(int num) {
-        if(top2 - top1 > 1){
+        if(top2 - top1 > 1){ //checking empty space in array
             top1++;
             arr[top1]=num;
         }
-      
+   
     }
 
     // Push in stack 2.
-    void push2(int num) {
+    void push2(int num) { 
+        //first element from stack2 will be inserted at index arr[size-1];
         if(top2 - top1 > 1){
              top2--;
             arr[top2]=num;   
@@ -42,7 +43,7 @@ public:
     }
 
     // Pop from stack 2 and return popped element.
-    int pop2() {
+    int pop2() { //pop operation in stack2 increse top2 by 1 till size-1
         if(top2 < size){
            int res=arr[top2];
            top2++;
