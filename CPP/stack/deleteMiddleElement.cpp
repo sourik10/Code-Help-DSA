6e9615b443d8afc644dc2c9c0c3a002f984f58af
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-02-18 00:46:00 +0530

    Update deleteMiddleElement.cpp

--- CPP/stack/deleteMiddleElement.cpp
+++ CPP/stack/deleteMiddleElement.cpp
@@ -1,10 +1,16 @@
+//approach 
+
 void solve(stack<int>&st,int ct,int N){
+    
+    //base case upto size / 2 
     if(ct==N/2){
         st.pop();
         return;
     }
     int t=st.top();
     st.pop();
+    
+    //recrusive traverse from stack top to size / 2 
     solve(st,ct+1,N);
     st.push(t);
 }
